d8c023a228c1b9efb951ea4c1c07cf0b5d0dd4db
João Assêncio <joao.assencio123@gmail.com>
2024-03-29 15:15:01 -0300

    Criado o método de seleção de candidatos

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         ProcessoSeletivo novoProcesso = new ProcessoSeletivo(1500);
-        novoProcesso.analisarCandidato();
+        novoProcesso.selecionarCandidato();
     }
 }
--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,19 +1,45 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     private float salarioPretendido;
+    float baseSalarial = 2000;
 
     public ProcessoSeletivo(float salarioPretendido) {
         this.salarioPretendido = salarioPretendido;
     }
 
     public void analisarCandidato() {
-        float baseSalarial = 2000;
-
         if (baseSalarial > salarioPretendido) {
             System.out.println("ligar para o candidato.");
         } else if (baseSalarial == salarioPretendido) {
             System.out.println("Ligar para o candidato com contra proposta.");
         } else {
             System.out.println("Aguardando resultado de demais candidatos.");
+        }
+    }
+
+    private static float valorPretendido() {
+        return ThreadLocalRandom.current().nextFloat(1800, 2200);
+    }
+
+    public void selecionarCandidato() {
+        String[] candidatos = {"João", "Jéssica", "Guilherme", "Débora", "Tavito", "Paulo", "Letícia", "Nicolas", "Giselle", "Lucas"};
+        int candidatosSelecionados = 0;
+        int indiceCandidatos = 0;
+
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[indiceCandidatos];
+            salarioPretendido = valorPretendido();
+
+            System.out.printf(candidato + " solicitou o valor R$ %.2f\n", salarioPretendido);
+
+            if (baseSalarial >= salarioPretendido) {
+                System.out.println(candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            } else {
+                System.out.println(candidato + " não foi selecionado para a vaga.");
+            }
+            indiceCandidatos++;
         }
     }
 }
